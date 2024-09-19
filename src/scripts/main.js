0aa2a5f0eda2c2c91ab33ad73c9d28f8fd30eb39
luwarnava <128074585+luwarnava@users.noreply.github.com>
2024-09-19 15:08:17 -0300

    criando o layout

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+const dataDoEvento = new Date("Sep 29, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horasEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horasEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
